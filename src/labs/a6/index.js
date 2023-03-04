602690d9bfc6ac3203b6498195c2476d5b46b975
kirangregerson <kirangregerson@gmail.com>
2023-03-03 20:06:23 -0500

    Update index.js

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -2,7 +2,6 @@ import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput
     from "./conditional-output";
-import TodoItem from "./todo/todo-item";
 import TodoList from "./todo/todo-list";
 
 function Assignment6() {
